230674db0cd439b5dc1ef4afef3287d0ef289050
Sylvia-iannarelli <sylvia_iannarelli@hotmail.com>
2024-07-30 11:16:09 +0200

    2.1

--- Exercices Refactorisation/Exercice 2/1. Single Responsability/SingleResponsability.php
+++ Exercices Refactorisation/Exercice 2/1. Single Responsability/SingleResponsability.php
@@ -2,7 +2,36 @@
 
 namespace SRPViolation;
 
-class Modem
+interface CanDial {
+    public function dial($pno);
+}
+
+interface CanHangup {
+    public function hangup();
+}
+
+interface CanSend {
+    public function send($c);
+}
+
+interface CanReceive {
+    public function receive();
+}
+
+class DataChannel implements CanSend, CanReceive
+{
+    public function send($c)
+    {
+        // Implementing send($c) method.
+    }
+
+    public function receive()
+    {
+        // Implementing receive() method.
+    }
+}
+
+class Connexion implements CanDial, CanHangup
 {
     public function dial($pno)
     {
@@ -12,15 +41,5 @@ class Modem
     public function hangup()
     {
         //  Implementing hangup() method.
-    }
-
-    public function send($c)
-    {
-        // Implementing send($c) method.
-    }
-
-    public function receive()
-    {
-        // Implementing receive() method.
     }
 }
