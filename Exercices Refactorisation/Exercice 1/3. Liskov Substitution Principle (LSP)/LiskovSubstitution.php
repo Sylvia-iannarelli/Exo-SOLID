8c17b28ad21cb62ff597b988aeefc9db1c6ed959
Sylvia-iannarelli <sylvia_iannarelli@hotmail.com>
2024-07-30 09:35:02 +0200

    1.3

--- Exercices Refactorisation/Exercice 1/3. Liskov Substitution Principle (LSP)/LiskovSubstitution.php
+++ Exercices Refactorisation/Exercice 1/3. Liskov Substitution Principle (LSP)/LiskovSubstitution.php
@@ -36,17 +36,6 @@ class Rectangle
 
 class Square extends Rectangle {
     
-	public function setWidth($width) 
-	{
-		$this->width = $width;
-		$this->height = $width;
-	}
-	
-	public function setHeight($height) 
-	{
-		$this->width = $height;
-		$this->height = $height;
-	}
 }
 
 class GraphicEditor
