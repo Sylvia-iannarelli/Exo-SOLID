95694c6a610da6532bbefbf87c159b2223711974
Sylvia-iannarelli <sylvia_iannarelli@hotmail.com>
2024-07-30 11:03:20 +0200

    1.5

--- Exercices Refactorisation/Exercice 1/5. Depency Inversion Principle (DIP)/DependencyInjection.php
+++ Exercices Refactorisation/Exercice 1/5. Depency Inversion Principle (DIP)/DependencyInjection.php
@@ -2,23 +2,26 @@
 
 // TODO refactoriser le code pour respecter le Principe de d'Invertion de Dépendence de la programmation SOLID
 
+interface ComputerInterface {
+    public function on();
+}
 
-class Computer
+class Computer implements ComputerInterface
 {
     public function on() {}
 }
 
 class Button 
 {
-    /**
-     * @var Computer
-     */
-    private $computer;
+    private $computerInterface;
+    public function __construct(ComputerInterface $computerInterface) {
+        $this->computerInterface = $computerInterface;
+    }
 
     public function activate()
     {
         if (condition) { //some condition
-            $this->computer->on();
+            $this->computerInterface->on();
         }
     }
 }
--- Exercices Refactorisation/Exercice 1/5. Depency Inversion Principle (DIP)/ReadMe.md
+++ Exercices Refactorisation/Exercice 1/5. Depency Inversion Principle (DIP)/ReadMe.md
@@ -1,4 +1,3 @@
 ### EXPLICATIONS
 
-
 Dans l'exemple ci-dessous, nous pouvons voir que le bouton dépend d'une classe spécifique "Ordinateur" et connaît les détails de sa mise en œuvre violant le DIP qui stipule que les détails doivent dépendre des abstractions.
